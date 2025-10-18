24782195e9763ac570bb438110287bb542b84a01
mousemckill <mousemckill@gmail.com>
2025-10-18 16:13:43 +0400

    test: show junit report

--- playwright.config.ts
+++ playwright.config.ts
@@ -23,20 +23,7 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: [
-    ['html'],
-    // ...(process.env.CI ? [
-    //   ['github'], 
-    //   ['@estruyf/github-actions-reporter', <GitHubActionOptions>{
-    //     useDetails: true,
-    //     showError: true,
-    //     showArtifactsLink: true,
-    //     showAnnotationsInColumn: true,
-    //     quiet: true
-    //   }]
-    // ] as ReporterDescription[] 
-    // : []),
-  ],
+  reporter: [['json', { outputFile: 'results.json' }], ['junit', { outputFile: 'results.xml' }]],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
